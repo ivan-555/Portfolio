10accd390448f998206e9106434b9c3e53ed3bc4
Ivan Facchinetti <ivan.facchinetti55@gmail.com>
2025-06-08 21:17:26 +0200

    Lighthouse optimization

--- js/preloader.js
+++ js/preloader.js
@@ -3,9 +3,11 @@ const landingPage = document.querySelector(".page.about");
 const sidebar = document.querySelector(".sidebar");
 
 window.addEventListener("load", () => {
-  // Hide preloader
-  preloader.classList.add("hidden")
-  // Show landing page and sidebar
-  landingPage.classList.add("active");
-  sidebar.style.display = "flex";
-})
+  setTimeout(() => {
+    // Hide preloader
+    preloader.classList.add("hidden")
+    // Show landing page and sidebar
+    landingPage.classList.add("active");
+    sidebar.style.display = "flex";
+  })
+}, 200)
